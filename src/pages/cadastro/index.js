1647c24767221050e2ceee42e3315dd032c7e9f5
Gabriel lima <87135960+Lima404@users.noreply.github.com>
2023-12-10 15:59:28 -0300

    pages cadastro

--- src/pages/cadastro/index.js
+++ src/pages/cadastro/index.js
@@ -1,53 +1,80 @@
 import styles from './cadastro.module.css'
 import CampoTexto from '@/components/campotexto/campo'
-import Botao from '@/components/botao'
 import Image from 'next/image'
 import { useState } from 'react'
 
 const Cadastro = () => {
-
     const [nome, setNome] = useState('')
     const [email, setEmail] = useState('')
     const [senha, setSenha] = useState('')
-    
-    
-    const aoSalvar = (evento) => {
-        evento.preventDefault();
-        console.log('Form foi submetido =>', nome, email, senha)
+
+    const handleCadastro = async () => {
+        const url = 'https://easytrade-backend-p5k1.onrender.com/usuarios/criar'
+
+        try {
+            const response = await fetch(url, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    nome: nome,
+                    email: email,
+                    senha: senha
+                }),
+            })
+
+            if (response.ok) {
+                console.log("cadastro bem sucedido")
+            } else {
+                console.error("falha no cadastro")
+            }
+        } catch (error) {
+            console.error("erro ao fazer cadastro", error)
+        }
     }
 
     return(
-        <section>
-            <form>
-                <h2>Cadastro</h2>
-                <div className={styles.box-interno}>
-                    <CampoTexto 
-                    prior={true} 
-                    label="Nome" 
-                    placeholder="Digite seu nome..."
-                    valor={nome}
-                    aoAlterado={valor => setNome(valor)}/>
-                    <CampoTexto 
-                    prior={true} 
-                    label="Email" 
-                    placeholder="Digite seu email..."
-                    valor={email}
-                    aoAlterado={valor => setEmail(valor)}/>
-                    <CampoTexto 
-                    prior={true} 
-                    label="Senha" 
-                    placeholder="Digite sua senha..."
-                    valor={senha}
-                    aoAlterado={valor => setSenha(valor)}/>
-                    <Botao>Enviar</Botao>
+        <div className={styles.container}>
+            <div className={styles.box}>
+                <div className={styles.form}>
+                    <div className={styles.box_interno}>
+                        <div className={styles.inputs}>
+                            <h2>Cadastro</h2>
+                            <CampoTexto
+                            prior={true}
+                            type= "Text"
+                            placeholder="Digite seu nome..."
+                            valor={nome}
+                            aoAlterado={valor => setNome(valor)}/>
+                            <CampoTexto
+                            prior={true}
+                            type= "Text"
+                            placeholder="Digite seu email..."
+                            valor={email}
+                            aoAlterado={valor => setEmail(valor)}/>
+                            <CampoTexto
+                            prior={true}
+                            type="password"
+                            placeholder="Digite sua senha..."
+                            valor={senha}
+                            aoAlterado={valor => setSenha(valor)}/>
+                            <button onClick={handleCadastro}>Fazer cadastro</button>
+                        </div>
+                    </div>
+                    <div className={styles.imagem}>
+                        <Image
+                        src="/imgcadastro.svg"
+                        alt='imgbackgroud cadastro'
+                        width={597}
+                        height={523}
+                        priority
+                        />
+                    </div>
                 </div>
-                <Image
-                src="/imgcadastro.svg"
-                alt='imgbackgroud cadastro'
-                width={508}
-                height={523}
-                />
-            </form>
-        </section>
+            </div>
+        </div>
     )
 }
+
+export default Cadastro
