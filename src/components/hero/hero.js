695b5b91a68f24e571c1ff6f5a903aadcea7e43d
Ricardo Alencar <ricfilho0007@gmail.com>
2023-12-11 20:31:37 -0300

    correções na página inicial

--- src/components/hero/hero.js
+++ src/components/hero/hero.js
@@ -4,24 +4,26 @@ import Link from 'next/link'
 
 export default function Hero() {
     return (
-        <div className={styles.hero}>
-            <section className={styles.title}>
-                <h1>EasyTrade</h1>
-                <p>Potencialize Seus Lucros: Simplificando o Gerenciamento de Vendas com Nosso Sistema Integrado</p>
-            </section>
-            <section className={styles.txtlogin}>
-                <p>Desbloqueie o potencial do seu negócio com um único clique. Faça login agora e impulsione suas vendas para o próximo nível!</p>
-                <Botao>
-                    <Link href="/login">
-                        Login
-                    </Link>
-                </Botao> 
-                <Botao>
-                    <Link href="/cadastro">
-                        Cadastro
-                    </Link>
-                </Botao>
-            </section>
+        <div className={styles.mainhero}>
+            <div className={styles.hero}>
+                <section className={styles.title}>
+                    <h1>EasyTrade</h1>
+                    <p>Potencialize Seus Lucros: Simplificando o Gerenciamento de Vendas com Nosso Sistema Integrado</p>
+                </section>
+                <section className={styles.txtlogin}>
+                    <p>Desbloqueie o potencial do seu negócio com um único clique. Faça login agora e impulsione suas vendas para o próximo nível!</p>
+                    <Botao>
+                        <Link href="/login">
+                            Login
+                        </Link>
+                    </Botao>
+                    <Botao>
+                        <Link href="/cadastro">
+                            Cadastro
+                        </Link>
+                    </Botao>
+                </section>
+            </div>
         </div>
     )
 }
