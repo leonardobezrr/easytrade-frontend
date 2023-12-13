7f0611a73c72178ae461e1f849fdd9f3564cf3b3
Ricardo Alencar <ricfilho0007@gmail.com>
2023-12-13 15:17:45 -0300

    mostrando produtos das vendas

--- src/pages/deskvendas/index.js
+++ src/pages/deskvendas/index.js
@@ -1,8 +1,10 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 
 export default function Vendas() {
-
     const [vendas, setVendas] = useState([]);
+    const [modalOpen, setModalOpen] = useState(false);
+    const [produtosDaVenda, setProdutosDaVenda] = useState([]);
+    const [selectedVendaId, setSelectedVendaId] = useState(null);
 
     useEffect(() => {
         const idUsuario = localStorage.getItem('user') ? JSON.parse(localStorage.getItem('user')).id : null;
@@ -29,33 +31,71 @@ export default function Vendas() {
         }
     };
 
-    return (
-        <table border={1}>
-            <tr>
-                <td>
-                    <th>ID</th>
-                </td>
-                <td>
-                    <th>Data da venda</th>
-                </td>
-                <td>
-                    <th>Valor da venda</th>
-                </td>
-                <td>
-                    <th>Ações</th>
-                </td>
-            </tr>
+    const fetchProdutosDaVenda = async (idVenda) => {
+        try {
+            const response = await fetch(`https://easytrade-backend-p5k1.onrender.com/produtosvenda/listar/${idVenda}`);
+            if (response.ok) {
+                const data = await response.json();
+                console.log('Produtos da venda:', data);
+                setProdutosDaVenda(data);
+            } else {
+                console.error('Falha ao obter produtos da venda:', response.statusText);
+            }
+        } catch (error) {
+            console.error('Erro ao fazer requisição de produtos da venda:', error);
+        }
+    };
 
-            {vendas.map((venda) => (
-                <tr key={venda.id}>
-                    <td>{venda.id}</td>
-                    <td>{venda.date}</td>
-                    <td>{venda.valor_venda}</td>
-                    <td>
-                        Editar | Excluir
-                    </td>
-                </tr>
-            ))}
-        </table>
-    )
+    const handleVerProdutosClick = (vendaId) => {
+        setSelectedVendaId(vendaId);
+        setModalOpen(true);
+        fetchProdutosDaVenda(vendaId);
+    };
+
+    const handleCloseModal = () => {
+        setModalOpen(false);
+        setSelectedVendaId(null);
+        setProdutosDaVenda([]);
+    };
+
+    return (
+        <div>
+            <table border={1}>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Data da venda</th>
+                        <th>Valor da venda</th>
+                        <th>Ações</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {vendas?.map((venda) => (
+                        <tr key={venda.id}>
+                            <td>{venda.id}</td>
+                            <td>{venda.date}</td>
+                            <td>{venda.valor_venda}</td>
+                            <td>
+                                <button onClick={() => handleVerProdutosClick(venda.id)}>ver produtos</button>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+
+            {modalOpen && (
+                <dialog open={modalOpen} onClose={handleCloseModal}>
+                    <div>
+                        <h2>Produtos da Venda</h2>
+                        <ul>
+                            {produtosDaVenda?.map((produto) => (
+                                <li key={produto.id}>{produto.id} | {produto.idproduto} | {produto.idvenda} | {produto.valor_unitario} | {produto.quantidade}</li>
+                            ))}
+                        </ul>
+                        <button onClick={handleCloseModal}>Fechar</button>
+                    </div>
+                </dialog>
+            )}
+        </div>
+    );
 }
