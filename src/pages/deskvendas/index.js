8015e6b49a114285729798d4efdc0b681dddfcb4
Gabriel lima <87135960+Lima404@users.noreply.github.com>
2023-12-13 15:51:47 -0300

    tela de vendas

--- src/pages/deskvendas/index.js
+++ src/pages/deskvendas/index.js
@@ -1,8 +1,20 @@
 import { useState, useEffect } from "react";
+import Image from "next/image";
+import style from "./vendas.module.css"
+import ListaSuspensa from "@/components/listasuspensa";
+import Link from "next/link";
 
 export default function Vendas() {
 
+    const filtros = [
+        'Data da venda',
+        'valor',
+        'produto',
+        'id',
+    ]
+
     const [vendas, setVendas] = useState([]);
+    const [filtro, setFiltros] = useState('')
 
     useEffect(() => {
         const idUsuario = localStorage.getItem('user') ? JSON.parse(localStorage.getItem('user')).id : null;
@@ -30,32 +42,80 @@ export default function Vendas() {
     };
 
     return (
-        <table border={1}>
-            <tr>
-                <td>
-                    <th>ID</th>
-                </td>
-                <td>
-                    <th>Data da venda</th>
-                </td>
-                <td>
-                    <th>Valor da venda</th>
-                </td>
-                <td>
-                    <th>Ações</th>
-                </td>
-            </tr>
-
-            {vendas.map((venda) => (
-                <tr key={venda.id}>
-                    <td>{venda.id}</td>
-                    <td>{venda.date}</td>
-                    <td>{venda.valor_venda}</td>
+        <div className={style.container}>
+            <div className={style.top}>
+                <div className={style.start}>
+                    <Link href="/home">
+                        <Image
+                        src="/menubuttom.svg"
+                        alt='menubuttom'
+                        width={24}
+                        height={22.9}
+                        priority/>
+                    </Link>
+                    <h2 className={style.h2}>Movimentação de caixa</h2>
+                </div>
+                <div className={style.end}>
+                    <h2>Nome de usuário</h2>
+                    <Image
+                    src="/pp.svg"
+                    alt='logo usurário'
+                    width={24}
+                    height={22.9}
+                    priority/>
+                    <Image
+                    src="/bell.svg"
+                    alt='logo notificação'
+                    width={24}
+                    height={22.9}
+                    priority/>
+                </div>
+            </div>
+            <div className={style.filtro}>
+                <div className={style.filter}>
+                <Image
+                    src="/filtro.svg"
+                    alt='logo notificação'
+                    width={24}
+                    height={22.9}
+                    priority/>
+                </div>
+                <p className={style.p1}>Filtrar por:</p>
+                <ListaSuspensa
+                itens={filtros}
+                valor={filtros}
+                aoAlterado={valor => setFiltros(valor)}/>
+                <p className={style.p1}>Valor:</p>
+                <label className={style.label}>
+                    <input className={style.input} placeholder='Valor' required={true} type="number" name="preco"/>
+                </label>
+            </div>
+            <table border={1}>
+                <tr>
                     <td>
-                        Editar | Excluir
+                        <th>ID</th>
+                    </td>
+                    <td>
+                        <th>Data da venda</th>
+                    </td>
+                    <td>
+                        <th>Valor da venda</th>
+                    </td>
+                    <td>
+                        <th>Ações</th>
                     </td>
                 </tr>
-            ))}
-        </table>
+                {vendas.map((venda) => (
+                    <tr key={venda.id}>
+                        <td>{venda.id}</td>
+                        <td>{venda.date}</td>
+                        <td>{venda.valor_venda}</td>
+                        <td>
+                            Editar | Excluir
+                        </td>
+                    </tr>
+                ))}
+            </table>
+        </div>
     )
 }
