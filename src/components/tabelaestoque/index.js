cf1b838cb820c1ed7f5830623a8f7643983b102b
Ricardo Alencar <ricfilho0007@gmail.com>
2023-12-12 23:32:36 -0300

    feat: buscando produtos do usuário

--- src/components/tabelaestoque/index.js
+++ src/components/tabelaestoque/index.js
@@ -1,43 +1,71 @@
 import style from "./tabelaest.module.css"
-import { useRouter } from 'next/router';
-import { useEffect, useState } from 'react';
+import { useState, useEffect } from "react";
 
-export default function Tabela(){
+export default function Tabela() {
 
-    return(
-        <div className={style.head}>
-            <section className={style.tabela}>
-                <div className={style.titles}>
-                    <div className={style.tit}>
-                        <h3 className={style.h3}>Item:</h3>
-                    </div>
-                    <p className={style.p}>Lorem Ipsum</p>
-                </div>
-                <div className={style.titles}>
-                    <div className={style.tit}>
-                        <h3 className={style.h3}>Quantidade</h3>
-                    </div>
-                    <p className={style.p}>Lorem Ipsum</p>
-                </div>
-                <div className={style.titles}>
-                    <div className={style.tit}>
-                        <h3 className={style.h3}>Descrição:</h3>
-                    </div>
-                    <p className={style.p}>Lorem Ipsum</p>
-                </div>
-                <div className={style.titles}>
-                    <div className={style.tit}>
-                        <h3 className={style.h3}>Preço:</h3>
-                    </div>
-                    <p className={style.p}>Lorem Ipsum</p>
-                </div>
-                <div className={style.titles}>
-                    <div className={style.tit}>
-                        <h3 className={style.h3}>Id°:</h3>
-                    </div>
-                    <p className={style.p}>Lorem Ipsum</p>
-                </div>
-            </section>
-        </div>
-    )
+    const [produtos, setProdutos] = useState([]);
+
+    useEffect(() => {l
+        const idUsuario = localStorage.getItem('user') ? JSON.parse(localStorage.getItem('user')).id : null;
+
+        if (idUsuario) {
+            fetchProdutos(idUsuario);
+        } else {
+            console.error('ID do usuário não encontrado.');
+        }
+    }, []);
+
+    const fetchProdutos = async (idUsuario) => {
+        try {
+            const response = await fetch(`https://easytrade-backend-p5k1.onrender.com/vendas/listar/${idUsuario}`);
+            if (response.ok) {
+                const data = await response.json();
+                console.log('Dados dos produtos:', data);
+                setProdutos(data);
+            } else {
+                console.error('Falha ao obter produtos:', response.statusText);
+            }
+        } catch (error) {
+            console.error('Erro ao fazer requisição de produtos:', error);
+        }
+    };
+
+    return (
+        <table border={1}>
+            <tr>
+                <td>
+                    <th>ID</th>
+                </td>
+                <td>
+                    <th>Nome</th>
+                </td>
+                <td>
+                    <th>Descrição</th>
+                </td>
+                <td>
+                    <th>Preço</th>
+                </td>
+                <td>
+                    <th>Quantidade em estoque</th>
+                </td>
+                <td>
+                    <th>Ações</th>
+                </td>
+            </tr>
+
+            {produtos.map((produto) => (
+                <tr key={produto.id}>
+                    <td>{produto.id}</td>
+                    <td>{produto.nome}</td>
+                    <td>{produto.descricao}</td>
+                    <td>{produto.preco}</td>
+                    <td>{produto.qtd_estoque}</td>
+                    <td>
+                        Editar | Excluir
+                    </td>
+                </tr>
+            ))}
+        </table>
+    );
+
 }
