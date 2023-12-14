d073f36a55302006606ea6e224c63eda22225617
Gabriel lima <87135960+Lima404@users.noreply.github.com>
2023-12-14 00:53:42 -0300

    update cadastro de venda js

--- src/pages/cadastrarvenda/index.js
+++ src/pages/cadastrarvenda/index.js
@@ -1,9 +1,35 @@
+import React, { useState } from 'react';
+import style from "./cadvenda.module.css"
 
 export default function CadastrarVenda() {
+
+    // onChange={handleChange} onSubmit={handleSubmit}value={formData.nome}
+// value={formData.descricao}
+// value={formData.preco}
+
     
     return(
-        <div>
-            cadastrar venda aqui
+
+        <div className={style.container}>
+            <form className={style.box}>
+                <h2 className={style.h2}>Cadastrar Venda</h2>
+                <div className={style.box_interno}>
+                    <label className={style.label}>
+                        <input className={style.input} placeholder='Data' required={true} type="text" name="Data"   />
+                    </label>
+                    <br />
+                    <label className={style.label}>
+                        <input className={style.input} placeholder='Valor' required={true} type="text" name="Valor"   />
+                    </label>
+                    <br />
+                    <label className={style.label}>
+                        <input className={style.input} placeholder='Produto' required={true} type="text" name="Produto"  />
+                    </label>
+                    <br />
+                    <br />
+                    <button className={style.but} type="submit">Cadastrar Produto</button>
+                </div>
+            </form>
         </div>
     )
 }
